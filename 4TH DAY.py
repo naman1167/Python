473b59067eaeb8a97a10d7adcdd5994779ac8f22
naman1167 <namansethi1167@gmail.com>
2025-01-30 11:17:26 +0530

    Simple Intreset in Python

---
+++ 4TH DAY.py
@@ -0,0 +1,26 @@
+# INPUT FUNCTIONSSSS!!!!!!!!!!!!
+name= input ("Enter Your Name")
+print ( " Your name is " , name )
+# aise sting me input lega
+age =int(input("Enter Your Age"))
+print (" your age is" , age )
+# or yeh aise age int lag dunga toh dung toh integer input leagaa..........
+contact =int(input('Enter Your Contact Number'))
+print('OK your number is ' , contact) 
+print(type(age))
+a= int(input('First Number' ))
+b = int(input("Enter your Second NUMber "))
+print(a+b)
+prncipal = input( "Enter the principal Amount ")
+RATE = input(' ENTER the RATE')
+TIME = input ("ENTER THE NUMBER OF YEARS")
+print ("interest is   " (prncipal*RATE*TIME/100))
+x , y , z = input(' Enter three values :') .split() 
+print(x , y , z )
+print (type(a))
+# IF ELSE STATEMENT !!!!!!!
+a = 100
+b= 20 
+min = "a is even" if a%2  else "a is odd"
+print( min )
+# nested loop is a loop inside a loop
