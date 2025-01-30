525e4f763d1604f900e2cf19a2d9d307e58ebca0
naman1167 <namansethi1167@gmail.com>
2025-01-30 11:15:34 +0530

    Python3

---
+++ 3rd day .py
@@ -0,0 +1,51 @@
+# operations in python 
+# Arthematic operations 
+print(7+5)
+print(7*5)
+print(7-5)
+print(7/5)
+# yeh sab operations ha
+print(8//3) # isme decimal part nhi milega 
+print(8%3) # isme mod milega
+print(8/-3)
+print(8%-3)
+# "%" iska matlab mod hoga 
+ # R= n-(d * floor(n/d ))
+ # yeh sab relational operators haaa!!!!
+print (8==8)
+print (8==0)
+print (8>=0)
+print (8!=0)
+print (8<=0)
+#   Logical Operators
+print (6>3 and 8>5)  # iske liye truth table samjhna padegaaa..... 
+print (6>3 or 8>5)
+print (6>8 or 8>9)
+print( not (8>9))
+a=5
+b=10
+a&b
+print(bin(a))
+# bin(a ) ise a ka binary mil jayegaa .!
+print(a&b)
+print (a|b)
+print(a<<1)
+print(b>>1)
+print(bin(8))
+print(bin(16))
+print(bin(17))
+print(bin(1))
+# Assigmnet Operators
+print(a++10)
+print(bin(-6))
+print(bin(6))
+n = [5,6,7,8,9,10,11]
+print(3  not in n)
+print( 11  not in n)
+print(9**2) # ** iska matlab exponential ha 
+# INPUT AND OUTPUT 
+print(' hello' , 10)
+print(' Result of experiment ', 10 , sep='is ' , end='90')
+print('')
+a='1909311654'
+print(a)
