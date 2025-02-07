d3e0a56ef8a2b15d72e13940fe4158fe032b78ae
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-02-07 10:01:36 +0530

    Add files via upload

---
+++ Day -11.py
@@ -0,0 +1,15 @@
+# student={"roll":101 , 'sname' : 'ajay' , 'contact': 9968874212 , 'fees':7867867}
+# print(student)
+# print(type(student))
+# # print (dir(student))
+# student2=student.copy()
+# print(student2)
+# print(student.pop('contact'))
+# print(student.pop('sname'))
+# print(student)
+# print(student2.keys())
+# print(sorted (student2)) 
+
+
+
+print("hello world !!!")
---
+++ day -9 .py
@@ -0,0 +1,25 @@
+# # G1=10
+# # def sample(n):
+# #     l1=0
+# #     global G2
+# #     G2=20
+# #     l1=l1+n+G1+G2
+# #     print(l1)
+
+# # sample(10)
+# # print("Global variable" , G1)
+# # print("Global variable within function" , G2)
+
+
+
+# # recurision is the process where the function call it self as its own body>>...>..> 
+
+# def sayhello(name):
+#     print("hello{}!! how are you ".format(name))
+#     return
+
+# def GB ():
+#     print("Good Bye !")
+
+
+
