9740ec2a2e561a972d9c6e00b0269ac9dadbcf7d
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-02-13 11:21:44 +0530

    Add files via upload

---
+++ PS (loops).py
@@ -0,0 +1,86 @@
+# loops PSl
+
+# table likhne ke liyee 
+
+'''n =  int(input("Enter the number: "))
+for i in range (1,11):
+    print(f"{n} X {i} = {n*i}")'''
+# S naam walo ko greet karna ha !!!
+
+'''l=["Naman","Piyush","Soham","Shivam","Shiva"]
+
+for name in l :
+    if (name.startswith("S")):
+        print("Hello",name ) '''
+
+'''n= int(input("Enter the number: "))
+i = 1
+
+while (i<11):
+    print(f"{n} X {i} = {n*i}")
+    i =+1
+    
+print '''
+
+# to check if the given number is prime or not  !!!
+
+n = int(input("Enter a number:  "))
+
+for i in range (2,n):
+    if (n%i) == 0 :
+        print("The no. is not prime")
+else :
+    print('prime number ')
+
+
+# to get the sum of first n natural numbers !!! using while loop ...
+'''n= int(input("Enter the number "))
+i = 1
+sum = 0
+while (i<=n):
+    sum += i
+    i+=1
+
+print(sum) '''
+
+
+# to calculate the factorial with for loop
+
+'''n = int(input("Enter the number:  "))
+product = 1  # Yeh hamne kudh definre kiya ha !!!!
+for i in range (1,n+1):
+    product = product * i
+
+print(f"the factorial of {n} is {product}" ) '''
+
+
+# Calculator using python !!!
+
+'''a = int(input("Enter first number "))
+b = int(input("Enter Second number "))
+n = input("Enter the operation ")
+
+if (n == "+"):
+    print(a+b)
+
+elif (n == "-"):
+    print(a-b)
+
+elif (n == "*" ):
+    print(a*b)
+
+else :
+    print ("Invalid operation") '''
+
+# to check the no. is odd or even ??
+
+'''a = int(input("Enter the number : "))
+if  a %2 ==0 :
+    print ("Even NUmber ")
+
+else :
+    print ("oDD")'''
+    
+
+ 
+
---
+++ Ps (List and tupples).py
@@ -0,0 +1,36 @@
+'''fruits = []
+
+f1= input("enter fruit name:  ")
+fruits.append(f1)
+f2= input("enter fruit name:  ")
+fruits.append(f2)
+f3= input("enter fruit name:  ")
+fruits.append(f3)
+f4= input("enter fruit name:  ")
+fruits.append(f4)
+print(fruits)  '''
+
+'''marks = []
+m1=input("Enter Marks : ")
+marks.append(m1)
+m2=input("Enter Marks : ")
+marks.append(m2)
+m3=input("Enter Marks : ")
+marks.append(m3)
+m4=input("Enter Marks : ")
+marks.append(m4)
+
+marks.sort()
+
+print(marks)'''
+'''
+l= [67,98,90,45] # yeh list ha bhai [] aise list bnegi
+print(sum(l)) '''
+
+
+a = (7,0,8,0,0,9)
+n = a.count(0)
+print(n)
+
+
+
